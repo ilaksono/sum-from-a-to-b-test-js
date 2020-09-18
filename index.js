477688b8650642635325c54819d10b3c53b7ae1f
ilaksono <ian.laksono@gmail.com>
2020-09-18 16:21:19 +0000

    added sum recursion function

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 
 function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
+  if(fromN < toN)
+    return fromN + sum(fromN+1, toN);
+  return toN;  
 }
+// console.log(sum(3,7));
 
 module.exports = sum;
