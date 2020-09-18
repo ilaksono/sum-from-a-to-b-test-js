00028743f83401b8e5f64cfea813b7f27c4339e1
ilaksono <ian.laksono@gmail.com>
2020-09-18 16:23:32 +0000

    fixed linting issues

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-
-function sum(fromN, toN) {
-  if(fromN < toN)
-    return fromN + sum(fromN+1, toN);
-  return toN;  
-}
+const sum = function(fromN, toN) {
+  if (fromN < toN)
+    return fromN + sum(fromN + 1, toN);
+  return toN;
+};
 // console.log(sum(3,7));
 
 module.exports = sum;
